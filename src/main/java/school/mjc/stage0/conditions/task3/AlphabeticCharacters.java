e588c1ef44b67a1df334af8e667a7b389a99172c
kart <mkartveli9@gmail.com>
2023-02-18 16:48:56 +0000

    4.1

--- src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
@@ -2,5 +2,10 @@ package school.mjc.stage0.conditions.task3;
 
 public class AlphabeticCharacters {
     public void vowelDeterminer(char character) {
+        if (Character.isLetter(character)) {
+            System.out.println("English");
+        } else {
+            System.out.println("Non English");
+        }
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task3/Seasons.java
+++ src/main/java/school/mjc/stage0/conditions/task3/Seasons.java
@@ -2,6 +2,17 @@ package school.mjc.stage0.conditions.task3;
 
 public class Seasons {
     public void tellTheSeasonByMonthNumber(int month) {
+        if ( month < 3 ||  month >= 1 || month == 12) {
+            System.out.println("Winter");
+        } if (month < 6 || month >= 4) {
+            System.out.println("Spring");
+        } if (month < 9 || month >= 6) {
+            System.out.println("Spring");
+        }if (month < 12 || month >= 9) {
+            System.out.println("Autumn");
+        }else {
+            System.out.println("Wrong number");
+        }
 
     }
 }
